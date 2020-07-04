967c16ccba1f15cc2e7c5207c71323e7da1ac8b5
Danish Mohammed <damo6513@colorado.edu>
2020-07-04 04:08:01 -0600

    building the landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,16 @@
-import React from "react"
-
-export default function Home() {
-  return <div>Hello world!</div>
+import React, { Component } from "react"
+import backgroundImage from "../../assets/background.png"
+import "../styles/pages/index.scss"
+export default class Home extends Component {
+  render() {
+    return (
+      <div className='main'>
+        <img src={backgroundImage} alt="Background Image" />
+        <div className='header'>
+          <div>Hi, I'm Danish ;)</div>
+        </div>
+        
+      </div>
+    )
+  }
 }
